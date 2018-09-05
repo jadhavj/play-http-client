539b4234040094ab2b282999b4f7b21d7df43766
Jayesh Jadhav <jadhavj@users.noreply.github.com>
2018-09-05 20:21:03 +0530

    okd

--- target/scala-2.11/routes/main/router/Routes.scala
+++ target/scala-2.11/routes/main/router/Routes.scala
@@ -1,6 +1,10 @@
 // @GENERATOR:play-routes-compiler
 // @SOURCE:D:/play-http-client/conf/routes
+<<<<<<< HEAD
 // @DATE:Wed Sep 05 17:59:29 IST 2018
+=======
+// @DATE:Fri Aug 31 23:35:32 IST 2018
+>>>>>>> parent of 91dd95f... okd
 
 package router
 
